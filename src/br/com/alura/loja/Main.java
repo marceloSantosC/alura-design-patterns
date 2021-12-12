b97c2fa8f30f9b44e058cdf3d69d6151c7291cd3
Marcelo dos Santos <marcelo17082000@gmail.com>
2021-12-11 21:06:15 -0300

    Implementado cálculo de desconto com problemas

--- src/br/com/alura/loja/Main.java
+++ src/br/com/alura/loja/Main.java
@@ -1,9 +1,6 @@
 package br.com.alura.loja;
 
-import br.com.alura.loja.imposto.CalculoImpostoService;
-import br.com.alura.loja.imposto.ICMS;
-import br.com.alura.loja.imposto.ISS;
-import br.com.alura.loja.imposto.Imposto;
+import br.com.alura.loja.desconto.CalculoDescontoService;
 import br.com.alura.loja.orcamento.Orcamento;
 
 import java.math.BigDecimal;
@@ -11,16 +8,10 @@ import java.math.BigDecimal;
 public class Main {
     public static void main(String[] args) {
 
-        Orcamento orcamento = new Orcamento(new BigDecimal("100.00"));
-        CalculoImpostoService calculoImpostoService = new CalculoImpostoService();
+        Orcamento orcamento = new Orcamento(new BigDecimal("100.00"), 10);
 
-        Imposto imposto = new ICMS();
-
-        BigDecimal valorICMS = calculoImpostoService.calcularImposto(orcamento, imposto);
-        System.out.println(valorICMS);
-
-        imposto = new ISS();
-        BigDecimal valorISS = calculoImpostoService.calcularImposto(orcamento, imposto);
-        System.out.println(valorISS);
+        CalculoDescontoService calculoDescontoService = new CalculoDescontoService();
+        BigDecimal valorDesconto = calculoDescontoService.calcular(orcamento);
+        System.out.println(valorDesconto);
     }
 }
--- src/br/com/alura/loja/orcamento/Orcamento.java
+++ src/br/com/alura/loja/orcamento/Orcamento.java
@@ -4,12 +4,18 @@ import java.math.BigDecimal;
 
 public class Orcamento {
     private final BigDecimal valor;
+    private final Integer quantidadeItens;
 
-    public Orcamento(BigDecimal valor) {
+    public Orcamento(BigDecimal valor, Integer quantidadeItens) {
         this.valor = valor;
+        this.quantidadeItens = quantidadeItens;
     }
 
     public BigDecimal getValor() {
         return valor;
+    }
+
+    public Integer getQuantidadeItens() {
+        return quantidadeItens;
     }
 }
